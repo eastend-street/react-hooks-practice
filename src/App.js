1419b8ca79f1dc756e607154111eda725438b231
eastend-street <ss_1322irf@icloud.com>
2019-10-02 16:11:01 -0700

    added reset and double button

--- src/App.js
+++ src/App.js
@@ -5,11 +5,28 @@ const App = () => {
 
   const increment = () => setCount(count + 1);
   const decrement = () => setCount(count - 1);
+
+  const increment2 = () => setCount(previousCount => previousCount + 1);
+  const decrement2 = () => setCount(previousCount => previousCount - 1);
+
+  const reset = () => setCount(0);
+  const double = () => setCount(count * 2);
+
   return (
     <>
       <div>count: {count}</div>
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>-1</button>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={double}>*2</button>
+      </div>
     </>
   );
 };
