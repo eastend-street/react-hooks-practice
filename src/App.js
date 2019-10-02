0b4fbed110ea18f53ab0b2a11b7bae449a54e276
eastend-street <ss_1322irf@icloud.com>
2019-10-02 00:05:43 -0700

    added counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <div>
-    this is a template for react app
-  </div>;
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  return (
+    <>
+      <div>count: {count}</div>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+    </>
+  );
 };
 
 export default App;
